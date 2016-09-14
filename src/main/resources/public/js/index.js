bd3a8af418b7701a7d039ca2d25f86496317eb38
ixx <285771659@qq.com>
2016-09-14 11:20:05 +0800

    实现最简单版本的拆分替换

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -2,9 +2,20 @@
  * Created by chuxianming on 16/9/13.
  */
 function executor(){
-    var it = $("#it").val();
-    var xsp = $("#xsp").val();
-    $("#ot").text(it);
+    var res = "";//拆分数据显示
+    var it = $("#it").val();//输入待处理文本一行
+    var xsp = $("#xsp").val();//拆分字符
+    var ots = $("#ots").val();//输出格式
+    if(!xsp){
+        return;
+    }
+    var its = it.split(xsp);
+    $(its).each(function(i, v){
+        res += i+":"+v+"<br/>";
+        ots = ots.split("$("+i+")").join(v);
+    });
+    $("#ot").html(ots);
+    $("#xspres").html(res);
 }
 $(document).ready(function(){
     $(".xx").bind("keyup",executor);
