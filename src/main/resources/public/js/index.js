6d9fe1c4409b8c2df590fa441d39ab121ee75c0a
ixx <285771659@qq.com>
2016-09-18 13:45:16 +0800

    添加子规则ui

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -1,6 +1,9 @@
 /**
  * Created by chuxianming on 16/9/13.
  */
+//全局变量区
+var index = 0;//添加自定义规则的索引数
+
 function executor(){
     var res = "";//拆分数据显示
     var it = $("#it").val();//输入待处理文本一行
@@ -17,6 +20,14 @@ function executor(){
     $("#ot").html(ots);
     $("#xspres").html(res);
 }
+function addxsp(){
+    $("#xspdiv").append('<p id="xspp'+index+'">将:<input class="xx" id="xspi'+index+'" type="text" width="50"/>按:<input class="xx" id="xsp'+index+'" type="text" width="50"/>分隔<button onclick="delxsp('+index+')">&nbsp;&nbsp;-&nbsp;&nbsp;</button></p>');
+    index++;
+}
+function delxsp(i){
+    $("#xspp"+i).remove();
+}
 $(document).ready(function(){
     $(".xx").bind("keyup",executor);
+    $("#ab").bind("click",addxsp);
 });
