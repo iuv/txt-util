52fb4d59310c7bb8d8f5d64fb6ef6dc0a3f2927f
ixx <cxmm@vip.qq.com>
2021-01-08 15:14:25 +0800

    添加在线版本

---
+++ public/js/index.js
@@ -0,0 +1,105 @@
+/**
+ * Created by ixx on 16/9/13.
+ */
+//全局变量区
+var index = 0;//添加自定义规则的索引数
+var splitArr = {};//存储拆分的数组
+var rule = {};//上传规则格式化
+
+function executor() {
+    var it = $("#it").val();//输入待处理文本一行
+    it = it.split("\n")[0]
+    // 调用基础拆分传入一行， true 表示刷新预览
+    executor_base(it, true);
+}
+function executor_base(it, type) {
+    splitArr = {};//清空缓存
+    rule = {"z":[]};
+    var res = "";//拆分数据显示
+    var xsp = $("#xsp").val();//拆分字符
+    var ots = $("#ots").val();//输出格式
+    var template = ots;
+    if (!xsp) {
+        return;
+    }
+    //根据换行拆分输入,多行处理
+    // var itLines = it.split("\n");
+    //主规则处理
+    var its = it.split(xsp);
+    rule["root"] = xsp;
+    $(its).each(function (i, v) {
+        res += formatRes("$(",i,v);
+    });
+    //子规则处理
+    for (var i = 0; i < index; i++) {
+        var xspi = "$("+$("#xspi" + i).val()+")";
+        var xspx = $("#xsp" + i).val();
+        rule.z.push(xspi+xspx);
+        if (xspi && xspx && splitArr[xspi]) {
+            var xspiFix = xspi.substr(0,xspi.length-1)+".";//去掉最后一个")"
+            var _tmp = splitArr[xspi].split(xspx);
+            $(_tmp).each(function (i, v) {
+                res += formatRes(xspiFix, i, v);
+            });
+        }
+    }
+    var ks = Object.keys(splitArr);
+    for(var i = ks.length; i>=0 ; i--){
+        if(type){
+            ots = ots.split(ks[i]).join("<span class='blc'>"+splitArr[ks[i]]+"</span>");
+        } else {
+            ots = ots.split(ks[i]).join(splitArr[ks[i]]);
+        }
+    }
+    if(type){
+        $("#ot").html(ots);
+        $("#xspres").html(res);
+        //上传属性设值
+        $("#template").val(template);
+        $("#rule").val(JSON.stringify(rule));
+        $("#show_rule").text(JSON.stringify(rule));
+        $("#show_rule_class").text(JSON.stringify(rule).split("\"").join("\\\""));
+    } else {
+        return ots;
+    }
+}
+//格式化输出拆分数据
+function formatRes(head, i, v){
+    var key = head + i + ")";
+    var res = "<span class='keyspan' onclick='setKey(\""+key+"\")'>" + key + "</span>:" + v + "<br/>";
+    splitArr[key] = v;
+    return res;
+}
+//在模板中设置key
+function setKey(key){
+    $("#ots").insertAtCaret(key);
+    executor();//重新输出内容
+}
+//添加子规则
+function addxsp() {
+    $("#xspdiv").append('<p id="xspp' + index + '">将:$(<input onkeyup="executor()" id="xspi' + index + '" type="text" width="50"/>)按:<input onkeyup="executor()" id="xsp' + index + '" type="text" width="50"/>分隔<button onclick="delxsp(' + index + ')">&nbsp;&nbsp;-&nbsp;&nbsp;</button></p>');
+    index++;
+}
+//删除子规则
+function delxsp(i) {
+    $("#xspp" + i).remove();
+    executor();
+}
+// 处理输出
+function execOut(){
+    var it = $("#it").val();//输入待处理文本一行
+    var its = it.split("\n")
+    var out_txt = "";
+    for(var i =0; i< its.length; i++){
+        // 执行不显示刷新
+        if(its[i] && its[i].trim()){
+            out_txt += executor_base(its[i], false)+"\n";
+        }
+    }
+    $("#out_txt").val(out_txt);
+}
+//绑定事件
+$(document).ready(function () {
+    $(".xx").bind("keyup", executor);
+    $("#ab").bind("click", addxsp);
+});
---
+++ public/readme.md
@@ -0,0 +1,81 @@
+# txt-util    
+[![License](https://img.shields.io/badge/license-Apache%202-4EB1BA.svg)](https://www.apache.org/licenses/LICENSE-2.0.html)
+[![Maven Central](https://maven-badges.herokuapp.com/maven-central/com.jianpage/txt-util/badge.svg)](https://maven-badges.herokuapp.com/maven-central/com.jianpage/txt-util)
+
+简单的文本处理小程序
+
+## 程序引用方法
+### 1.在项目中添加maven依赖 (或者直接下载build下的txt-util-0.1-RELEASE.jar)包
+ 
+```xml
+        <dependency>
+            <groupId>com.jianpage</groupId>
+            <artifactId>txt-util</artifactId>
+            <version>0.1-RELEASE</version>
+        </dependency>
+        <dependency>
+            <groupId>org.slf4j</groupId>
+            <artifactId>slf4j-api</artifactId>
+            <version>1.7.21</version>
+        </dependency>
+        <dependency>
+            <groupId>log4j</groupId>
+            <artifactId>log4j</artifactId>
+            <version>1.2.17</version>
+        </dependency>
+        <dependency>
+            <groupId>com.alibaba</groupId>
+            <artifactId>fastjson</artifactId>
+            <version>1.2.66</version>
+        </dependency>
+```
+
+如果已有slf4j和fastjson依赖则只添加txt-util即可
+
+### 2.添加资源文件txtutil.properties 
+
+在项目资源目录下添加 `txtutil.properties` 文件,内容格式如:
+```
+test={"z":["$(0);"],"root":","}
+```
+
+可在页面配置规则时获取输出的规则
+
+### 3.程序中引用 
+
+```java
+/**
+* 参数 配置规则key, 输出内容模板, 需要处理内容
+* 返回 按模板输出处理后的内容String
+*/
+TxtUtil.handle("test", template, line);
+```
+
+## 运行环境
+>jdk1.7+ 
+>maven3+
+
+## 程序启动
+可以使用如下方法运行本程序
+
+* 直接下载build目录下的对应版本txt-util.jar到本地(基于jdk1.8版本编译请确认本地使用版本,如底于1.8请自行编译)
+
+    跳转到保存目录后使用java -jar txt-util.jar 命令启动服务
+
+* 或者使用git clone 下载本资源库
+
+    下载后进入项目目录执行 mvn 命令打包,再使用java -jar target/txt-util***.jar 启动服务
+
+程序默认使用8000端口,如需要修改可以修改源码中的application.properties
+或者在jar包同目录下创建application.properties文件,添加内容:
+>server.port=8000
+
+端口号改为自己需要的端口,启动使用使用 java -jar txt-util.jar --spring.config.location=./application.properties
+
+
+启动完成后在浏览器中输入 http://localhost:8000 打开程序
+
+## 界面说明
+* 截图
+
+![界面](build/vm.png)
